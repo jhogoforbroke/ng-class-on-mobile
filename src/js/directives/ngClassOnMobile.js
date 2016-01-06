95e6e70d9fecc442f8b8283c64be6105cbc67705
Jho <jmarinho87@live.com>
2016-01-05 22:03:44 -0200

    building a directive to change classes

--- src/js/directives/ngClassOnMobile.js
+++ src/js/directives/ngClassOnMobile.js
@@ -4,19 +4,39 @@ angular
   .module('ngClassOnMobile')
   .directive('ngClassOnMobile', ngClassOnMobile);
 
-ngClassOnMobile.$inject = ['$window'];
+ngClassOnMobile.$inject = ['ngClassOnMobile.deviceCheckService'];
 
-function ngClassOnMobile($window) {
+function ngClassOnMobile(deviceCheckService) {
   var directive = {
     restrict: 'A',
     link: link,
     scope: {
+      classToAdd: '@',
+      classToRemove: '@',
+      removeCssInline: '=',
     }
   };
 
   return directive;
 
   function link(scope, element, attrs) {
+
+    scope.classesToAdd = scope.classToAdd.split(', ').map(function(x){ return x.trim(); });
+    scope.classesToRemove = scope.classToRemove.split(', ').map(function(x){ return x.trim(); });
+
+    scope.classesToRemove.forEach(function(x){
+      element.removeClass(x);
+    });
+
+    scope.classesToAdd.forEach(function(x){
+      element.addClass(x);
+    });
+
+    if (scope.removeCssInline) {
+      element.removeProp('style');
+      element.removeAttr('style');
+    }
+
   }
 
 };
